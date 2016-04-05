a6a095e64cf538136183858d112963353805e3e8
LiZhang <lizhang@lizhangdemacbook-pro.local>
2016-04-05 15:40:50 +0800

    Initial Commit

---
+++ April05RefreshDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  April05RefreshDemo
+//
+//  Created by LiZhang on 16/4/5.
+//  Copyright © 2016年 LiZhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
